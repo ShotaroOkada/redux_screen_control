717526c3cc597394ae1620b79d5fb3600ce9063f
OkadaShotaro <b1016117@fun.ac.jp>
2019-02-03 15:10:12 +0900

    reducerの実装

---
+++ src/reducers/Screen/ScreenReducer.ts
@@ -0,0 +1,27 @@
+import { IScreenState, ScreenType } from "../../states/ScreenState";
+import ScreenAction from '../../actions/Screen/ScreenAction';
+import ScreenActiontype from '../../actions/Screen/ScreenActiontype';
+
+const initialState: IScreenState = {
+    screenType: ScreenType.HOME_SCREEN
+}
+
+const screenReducer = (state: IScreenState = initialState, action: ScreenAction): IScreenState => {
+    switch(action.type){
+        case ScreenActiontype.HOME_SCREEN:
+        return{
+            ...state,
+            screenType: ScreenType.MENU_SCREEN
+        };
+        case ScreenActiontype.MENU_SCREEN:
+        return{
+            ...state,
+            screenType: ScreenType.MENU_SCREEN
+        };
+        default:
+        return initialState;
+        
+    }
+};
+
+export default screenReducer;
